e1db8a84d89a8c31205c3450e466720773a70ccc
thordurf <thordur921@gmail.com>
2022-08-24 20:25:50 +0200

    minor fixes

--- script.js
+++ script.js
@@ -18,45 +18,22 @@ function handleNames(data){
 }
 
 function showName(name) {
-    //grab the template
+
     const template = document.querySelector("template").content;
-    //clone it
+
     const copy = template.cloneNode(true);
-    //change content
+
     copy.querySelector(".popup").textContent = name.fullname;
     copy.querySelector(".popuptext").textContent = name.movie;
    
-    function firstName() {
-        let actor = name.fullname;
-        let firstName = actor.split(" ")[0]
-        console.log(firstName)
-        copy.querySelector(".popup").classList.add(firstName);
-    }
-    firstName();
+    // function firstName() {
+    //     let actor = name.fullname;
+    //     let firstName = actor.split(" ")[0]
+    //     console.log(firstName)
+    //     copy.querySelector(".popup").classList.add(firstName);
+    // }
+    // firstName();
 
-    // copy.querySelector(".popup").classList.add(name.fullname);
-
-
-    //grab parent
     const parent = document.querySelector("main");
-    //append
-    parent.appendChild(copy);
-
-    
+    parent.appendChild(copy);   
 }
-
-// function makeCard() {
-//     const templateElement = document.querySelector("template").content;
-
-//     const myClone = templateElement.cloneNode(true);
-    
-//     myClone.querySelector(".popup").textContent = "Hi Mom";
-//     myClone.querySelector(".popuptext").textContent = "Hi Dad";
-
-    
-//     const parentElement = document.querySelector("main");
-    
-//     parentElement.appendChild(myClone);
-// }
-
-// makeCard();
